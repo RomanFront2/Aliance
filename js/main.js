11ecbaa8a173154f28b407dfc974a64d20779376
Davydov Roman <rdfrontend1@gmail.com>
2025-11-23 23:32:57 +0300

    add: header adaptive

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 const navbar = document.querySelector('.navbar')
 const logo = document.querySelector('.logo-svg use')
+const mobileMenuToggle = document.querySelector('.mobile-menu-toggle')
+const mobileMenu = document.querySelector('.mobile-menu')
 
 window.addEventListener('scroll', () => {
   if (this.scrollY > 1) {
@@ -9,4 +11,9 @@ window.addEventListener('scroll', () => {
     navbar.classList.remove("navbar-light")
     logo.href.baseVal = "img/sprite.svg#logo_light"
   }
+})
+
+mobileMenuToggle.addEventListener("click", (event) => {
+  event.preventDefault();
+  mobileMenu.classList.toggle("isOpen");
 })
