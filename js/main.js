fd5728d8015033c3c04b5aa87db8df9584ef296a
Davydov Roman <rdfrontend1@gmail.com>
2025-11-24 17:29:26 +0300

    add: js - switch mobile menu

--- js/main.js
+++ js/main.js
@@ -3,17 +3,36 @@ const logo = document.querySelector('.logo-svg use')
 const mobileMenuToggle = document.querySelector('.mobile-menu-toggle')
 const mobileMenu = document.querySelector('.mobile-menu')
 
+const lightModeOn = (event) => {
+  navbar.classList.add("navbar-light")
+  logo.href.baseVal = "img/sprite.svg#logo"
+}
+
+const lightModeOff = (event) => {
+  navbar.classList.remove("navbar-light")
+  logo.href.baseVal = "img/sprite.svg#logo_light"
+}
+
+const openMenu = (event) => {
+  mobileMenu.classList.add("isOpen");
+  mobileMenuToggle.classList.add("closeMenu");
+  document.body.style.overflow = "hidden";
+  lightModeOn()
+}
+
+const closeMenu = (event) => {
+  mobileMenu.classList.remove("isOpen");
+  mobileMenuToggle.classList.remove("closeMenu");
+  document.body.style.overflow = "";
+  lightModeOff()
+}
+
+
 window.addEventListener('scroll', () => {
-  if (this.scrollY > 1) {
-    navbar.classList.add("navbar-light")
-    logo.href.baseVal = "img/sprite.svg#logo"
-  } else {
-    navbar.classList.remove("navbar-light")
-    logo.href.baseVal = "img/sprite.svg#logo_light"
-  }
+  this.scrollY > 1 ? lightModeOn() : lightModeOff()
 })
 
 mobileMenuToggle.addEventListener("click", (event) => {
   event.preventDefault();
-  mobileMenu.classList.toggle("isOpen");
+  mobileMenu.classList.contains('isOpen') ? closeMenu() : openMenu();
 })
