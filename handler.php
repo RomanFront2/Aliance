e453d2f8e45ba6aefcde04821736faa3eb4df294
Davydov Roman <rdfrontend1@gmail.com>
2025-12-25 22:56:49 +0300

    upd delete token

--- handler.php
+++ handler.php
@@ -2,8 +2,33 @@
 $user_name = htmlspecialchars($_POST["username"]);
 $user_phone = htmlspecialchars($_POST["userphone"]);
 
-$token = "8265923983:AAGcPg_9AcpB1V4HVAgfbQDHRfhQZCSBHt4";
-$chat_id = "-5040493066";
+function setEnvData( string $filePath = '' ): void {
+	if ( empty( $filePath ) ) {
+		$filePath = '.env';
+	}
+	if ( ! file_exists( $filePath ) ) {
+		throw new RuntimeException( 'Wrong file path.' );
+	}
+	$file = file_get_contents( $filePath );
+	$data = array_filter( explode( PHP_EOL, $file ) );
+	if ( empty( $data ) ) {
+		return;
+	}
+	foreach ( $data as $i => $line ) {
+		$data[ $i ] = array_map( 'trim', explode( '=', $line, 2 ) );
+		if ( isset( $data[ $i ][1] ) ) {
+			$data[ $i ][1] = trim( $data[ $i ][1], '"' );
+		} else {
+			$data[ $i ][1] = '';
+		}
+		$_ENV[ $data[ $i ][0] ] = $data[ $i ][1];
+	}
+}
+
+setEnvData();
+
+$token= $_ENV['Bot_token'];
+$chat_id= $_ENV['Chat_id'];
 
 $form_date = array(
   "Клиент: " => $user_name,
@@ -21,6 +46,3 @@ if ($send_to_telegram) {
 } else {
   "Ошибочка";
 };
-
-// echo "Привет, " . $user_name . "<br>";
-// echo "Ваш телефон: <b>" . $user_phone . "</b>";
